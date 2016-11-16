e48a86f49e3a6a7917408f3a42fddeca1e353b23
humberto lopez <hlo8909@gmail.com>
2016-11-16 18:35:13 -0500

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>hloSubimgJs</title>
+	<title>hloformvalidatejs</title>
 	<link rel="stylesheet" type="text/css" href="css/materialize.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
